cf639ee0c2c944cb98834d35d6e4c2752a023f03
pnina1 <pnina@tickchak.co.il>
2023-08-29 20:18:38 +0300

    Updates

--- precache-manifest.1c08fe1fe12dda2a7842c644e29f8310.js
+++ precache-manifest.1c08fe1fe12dda2a7842c644e29f8310.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f8bdc8a46b4a99104f75a1b25fdebe4b",
+    "revision": "bead551be04d310ec1c97e08d354dcee",
     "url": "/tickchak-test/index.html"
   },
   {
-    "revision": "14f9f5e486c811c158d2",
+    "revision": "4a7ef9814c55db05c63b",
     "url": "/tickchak-test/static/css/main.cc6e32d2.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/tickchak-test/static/js/2.8e77d62c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "14f9f5e486c811c158d2",
-    "url": "/tickchak-test/static/js/main.7e0ac519.chunk.js"
+    "revision": "4a7ef9814c55db05c63b",
+    "url": "/tickchak-test/static/js/main.8b9f7884.chunk.js"
   },
   {
     "revision": "cbeee98de0577766da39",
